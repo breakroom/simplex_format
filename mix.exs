801943e3cb4ea4e8df50107021c3d10e3d289498
Tom Taylor <tom@tomtaylor.co.uk>
2019-02-15 09:47:47 +0000

    Add extra metadata for Hex

--- mix.exs
+++ mix.exs
@@ -6,8 +6,23 @@ defmodule SimpleFormat.MixProject do
       app: :simple_format,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: description(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "simple_format",
+      source_url: "https://github.com/poplarhq/simple_format"
+    ]
+  end
+
+  defp description do
+    "Convert plain text to formatted HTML, including automatically linking URLs"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/poplarhq/simple_format"}
     ]
   end
 
