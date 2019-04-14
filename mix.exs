1b4f7022b515b91eea27a993be5a79fd260ce518
Tom Taylor <tom@tomtaylor.co.uk>
2019-04-14 15:35:07 +0100

    Bump to version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimplexFormat.MixProject do
   def project do
     [
       app: :simplex_format,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       description: description(),
       start_permanent: Mix.env() == :prod,
